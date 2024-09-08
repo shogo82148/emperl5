b0764a287cc3fa2071e296f582af9e83d36c259b
ICHINOSE Shogo <shogo82148@gmail.com>
2024-09-08 10:14:52 +0900

    add hint file for emscripten

---
+++ hints/emscripten.sh
@@ -0,0 +1,25 @@
+osname="emscripten"
+archname="wasm"
+osvers="$(perl -e 'print qx(emcc --version)=~/(\d+\.\d+\.\d+)/')"
+
+myhostname='localhost'
+mydomain='.local'
+cf_email='shogo82148@gmail.com'
+perladmin='root@localhost'
+
+cc="emcc"
+ld="emcc"
+
+hostperl="/Users/shogo/src/github.com/Perl/perl5/miniperl"
+hostgenerate="/Users/shogo/src/github.com/Perl/perl5/generate_uudmap"
+
+nm="$(which llvm-nm)"  # note from Glossary: 'After Configure runs, the value is reset to a plain "nm" and is not useful.'
+ar="$(which llvm-ar)"  # note from Glossary: 'After Configure runs, the value is reset to a plain "ar" and is not useful.'
+ranlib="$(which llvm-ranlib)"
+
+man1dir="none"
+man3dir="none"
+
+usenm='undef'
+
+firstmakefile='GNUmakefile'
